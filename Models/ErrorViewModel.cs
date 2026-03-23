be32ef02c374bf7b5f0d23e159db9057bf1ff5a1
AjayAR <ajayperies10@gmail.com>
2026-03-23 13:23:35 +0530

    Initial commit: Created base MVC project with gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SecureFileShare.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
